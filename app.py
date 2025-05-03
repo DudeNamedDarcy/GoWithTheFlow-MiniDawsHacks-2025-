7b4456b8a00f79e19d5ea2805ba01ab737e56af5
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-05-03 10:52:34 -0400

    From OG Folder. Fixed IP address issue and starting to add Flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello!"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+#helped us fix this problem: https://stackoverflow.com/questions/70386660/flask-not-running
+#to run application, call "python -m flask --app .\app.py run"
+#Helpful vid: https://www.youtube.com/watch?v=6M3LzGmIAso
+
+
---
+++ views.py
@@ -0,0 +1,4 @@
+from flask import Blueprint #file is a Blueprint of our application AKA has a LOT OF URLS!
+
+views = Blueprint('views', __name__) #sets up a Blueprint for our Flask application
+
