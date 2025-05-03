65b35f8951ad941619fa0daaef17c4fdcd59b96b
Darcy Loane-Billings <122037471+DudeNamedDarcy@users.noreply.github.com>
2025-05-03 10:55:39 -0400

    Testing Changes to Localhost!

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello!"
+    return "Hello! This is an app!"
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=4000)
 #helped us fix this problem: https://stackoverflow.com/questions/70386660/flask-not-running
 #to run application, call "python -m flask --app .\app.py run"
 #Helpful vid: https://www.youtube.com/watch?v=6M3LzGmIAso
