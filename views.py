da4f7a4901e8c108794521cd7fca7e2672b183bc
Callahan-Vortex-11 <lowecallahan@gmail.com>
2025-05-03 14:17:08 -0400

    phone

--- views.py
+++ views.py
@@ -1,6 +1,17 @@
 from flask import Blueprint #file is a Blueprint of our application AKA has a LOT OF URLS!
 from flask import render_template #importing render_template allows us to RENDER HTML FILES TO ROUTES!
 from flask import request #importing request/importing from HTML forms!
+from twilio.rest import Client
+
+TWILIO_ACCOUNT_SID = ACe118c2c968320d9d1ef1856ec72f959a
+TWILIO_ACCOUNT_TOKEN = 69e8e58c52636e509d8bde12fa72ff8d
+TWILIO_PHONE_NUMBER = +15856591400
+TRUSTED_PHONE_NUMBER = 5149496480
+
+
+
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 views = Blueprint(__name__, 'views') #sets up a Blueprint for our Flask application
 
@@ -48,5 +59,18 @@ def retrieve_symptoms():
     if nausea == True:
         a +=1
     return "Stress Slider: ", a
+
+if a >= 5:
+    send_sms_alert
+
+def send_sms_alert():
+    #Sends sms to trusted individual
+    message_body = f"Alert: A person who has marked you as a trusted individual is experiencing a panic attack."
+
+    message = client.messages.create(
+        body=message_body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=TRUSTED_PHONE_NUMBER
+    )
 
 #Good source: https://www.geeksforgeeks.org/retrieving-html-from-data-using-flask/
